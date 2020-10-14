d76145b1e78f773211efff731ecfcb2dfb588a35
Zingabopp <Zingabopp@gmail.com>
2020-10-14 16:09:19 -0500

    Add project files.

---
+++ MultiplayerExtensions/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace MultiplayerExtensions
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        /// <summary>
+        /// Use to send log messages through BSIPA.
+        /// </summary>
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Plugin.Log.Info("OnApplicationStart");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+
+    }
+}
