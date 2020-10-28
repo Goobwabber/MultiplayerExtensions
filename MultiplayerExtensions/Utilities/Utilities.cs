060bd33cb621f816368aec8ace14870690b184ac
Zingabopp <Zingabopp@gmail.com>
2020-10-28 18:28:20 -0500

    Added LevelIdToHash function in Utilities

--- MultiplayerExtensions/Utilities/Utilities.cs
+++ MultiplayerExtensions/Utilities/Utilities.cs
@@ -46,5 +46,18 @@ namespace MultiplayerExtensions.Utilities
             }
 
         }
+
+        public static string? LevelIdToHash(string levelId)
+        {
+            if (string.IsNullOrWhiteSpace(levelId))
+                return null;
+            string[] ary = levelId.Split('_', ' ');
+            string? hash = null;
+            if (ary.Length > 2)
+                hash = ary[2];
+            if ((hash?.Length ?? 0) == 40)
+                return hash;
+            return null;
+        }
     }
 }
