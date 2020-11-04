86e839e57df78b0821f14037e238173dab6b6ab1
MT <littlehappytroll@gmail.com>
2020-11-04 13:26:36 -0500

    Refactored Custom Packet Handling

---
+++ MultiplayerExtensions/Networking/ExtendedPlayerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace MultiplayerExtensions.Networking
+{
+    class ExtendedPlayerManager : IInitializable
+    {
+        [Inject]
+        private ExtendedSessionManager _sessionManager;
+
+        public string localPlatformID;
+
+        public void Initialize()
+        {
+            _sessionManager.playerConnectedEvent += OnPlayerConnected;
+            _sessionManager.RegisterCallback(ExtendedSessionManager.MessageType.PlayerUpdate, HandlePlayerPacket, new Func<ExtendedPlayerPacket>(ExtendedPlayerPacket.pool.Obtain));
+
+            BS_Utils.Gameplay.GetUserInfo.GetUserAsync().ContinueWith(r =>
+            {
+                localPlatformID = r.Result.platformUserId;
+            });
+        }
+
+        private void OnPlayerConnected(ExtendedPlayer player)
+        {
+            if (localPlatformID != null)
+            {
+                ExtendedPlayerPacket localPlayerPacket = new ExtendedPlayerPacket().Init(localPlatformID);
+                _sessionManager.Send(localPlayerPacket);
+            }
+        }
+
+        private void HandlePlayerPacket(ExtendedPlayerPacket packet, ExtendedPlayer player)
+        {
+            player.platformID = packet.platformID;
+        }
+    }
+}
---
+++ MultiplayerExtensions/Networking/ExtendedPlayerPacket.cs
@@ -0,0 +1,44 @@
+﻿using LiteNetLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerExtensions.Networking
+{
+    class ExtendedPlayerPacket : INetSerializable, IPoolablePacket
+    {
+        public static PacketPool<ExtendedPlayerPacket> pool
+        {
+            get
+            {
+                return ThreadStaticPacketPool<ExtendedPlayerPacket>.pool;
+            }
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(platformID);
+        }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            this.platformID = reader.GetString();
+        }
+
+        public void Release()
+        {
+            ExtendedPlayerPacket.pool.Release(this);
+        }
+
+        public ExtendedPlayerPacket Init(string platformID)
+        {
+            this.platformID = platformID;
+
+            return this;
+        }
+
+        public string platformID;
+    }
+}
