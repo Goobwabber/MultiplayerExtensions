51139eb3a9bbf4eddd3be29e24da30329c3e3dd6
Zingabopp <Zingabopp@gmail.com>
2020-11-25 03:40:00 -0600

    Reduce logging for repeated packet exceptions

--- MultiplayerExtensions/HarmonyPatches/LoggingPatch.cs
+++ MultiplayerExtensions/HarmonyPatches/LoggingPatch.cs
@@ -2,13 +2,11 @@
 using LiteNetLib;
 using LiteNetLib.Utils;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MultiplayerExtensions.HarmonyPatches
 {
@@ -17,7 +15,7 @@ namespace MultiplayerExtensions.HarmonyPatches
     class LoggingPatch
     {
         private static readonly MethodInfo _exceptionLogger = SymbolExtensions.GetMethodInfo(() => ExceptionLogger((IConnectedPlayer)null!, (Exception)null!));
-
+        private static ConcurrentDictionary<string, PlayerExceptionTracker> PlayerExceptions = new ConcurrentDictionary<string, PlayerExceptionTracker>();
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator gen)
         {
             Harmony.DEBUG = true;
@@ -25,9 +23,9 @@ namespace MultiplayerExtensions.HarmonyPatches
                 Plugin.Log?.Error($"Couldn't find _exceptionLogger");
             else
                 Plugin.Log?.Warn($"Applying transpiler.");
-            var localException = gen.DeclareLocal(typeof(Exception));
+            LocalBuilder? localException = gen.DeclareLocal(typeof(Exception));
             localException.SetLocalSymInfo("ex");
-            foreach (var code in instructions)
+            foreach (CodeInstruction? code in instructions)
             {
                 if (code.opcode == OpCodes.Pop)
                 {
@@ -68,8 +66,23 @@ namespace MultiplayerExtensions.HarmonyPatches
 
         private static void ExceptionLogger(IConnectedPlayer p, Exception ex)
         {
-            Plugin.Log?.Warn($"An exception was thrown processing a packet from player '{p?.userName ?? "<NULL>"}|{p?.userId ?? " < NULL > "}': {ex.Message}");
-            Plugin.Log?.Debug(ex);
+            var tracker = PlayerExceptions.GetOrAdd(p.userId, new PlayerExceptionTracker());
+            tracker.MaybeLogException(p, ex);
+        }
+
+        private class PlayerExceptionTracker
+        {
+            private ConcurrentDictionary<string, int> Exceptions = new ConcurrentDictionary<string, int>();
+
+            public void MaybeLogException(IConnectedPlayer p, Exception ex)
+            {
+                int exCount = Exceptions.AddOrUpdate(ex.Message, 0, (msg, old) => old + 1);
+                if (exCount % 20 == 0)
+                {
+                    Plugin.Log?.Warn($"An exception was thrown processing a packet from player '{p?.userName ?? "<NULL>"}|{p?.userId ?? " < NULL > "}' ({exCount + 1}): {ex.Message}");
+                    Plugin.Log?.Debug(ex);
+                }
+            }
         }
     }
 }
