082fa07583bb64d65f8ba1c46edeceea82432d04
MT <littlehappytroll@gmail.com>
2021-06-19 23:37:41 -0400

    networking done !

--- MultiplayerExtensions/Emotes/EmotePacket.cs
+++ MultiplayerExtensions/Emotes/EmotePacket.cs
@@ -1,4 +1,5 @@
 ﻿using LiteNetLib.Utils;
+using UnityEngine;
 
 namespace MultiplayerExtensions.Emotes
 {
@@ -9,13 +10,33 @@ namespace MultiplayerExtensions.Emotes
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(source);
+
+            writer.Put(position.x);
+            writer.Put(position.y);
+            writer.Put(position.z);
+
+            writer.Put(rotation.x);
+            writer.Put(rotation.y);
+            writer.Put(rotation.z);
+            writer.Put(rotation.w);
         }
 
         public void Deserialize(NetDataReader reader)
         {
             source = reader.GetString();
+
+            position.x = reader.GetFloat();
+            position.y = reader.GetFloat();
+            position.z = reader.GetFloat();
+
+            rotation.x = reader.GetFloat();
+            rotation.y = reader.GetFloat();
+            rotation.z = reader.GetFloat();
+            rotation.w = reader.GetFloat();
         }
 
         public string source = null!;
+        public Vector3 position;
+        public Quaternion rotation;
     }
 }
