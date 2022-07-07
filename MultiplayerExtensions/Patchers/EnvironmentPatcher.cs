5f7e71d6fefa9c0d6dffe1d708978b01b859c145
goobwabber <littlehappytroll@gmail.com>
2022-07-07 18:55:55 -0400

    custom colors patch moved to mpcore

--- MultiplayerExtensions/Patchers/EnvironmentPatcher.cs
+++ MultiplayerExtensions/Patchers/EnvironmentPatcher.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using IPA.Utilities;
 using SiraUtil.Affinity;
 using SiraUtil.Logging;
@@ -218,39 +218,5 @@ namespace MultiplayerExtensions.Patchers
             _logger.Info($"Allow ring creation: {_allowRingCreation}");
             return _allowRingCreation;
         }
-
-        [AffinityPrefix]
-        [AffinityPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
-        private void CustomSongColorsPatch(ref IDifficultyBeatmap difficultyBeatmap, ref ColorScheme? overrideColorScheme)
-        {
-            if (!_config.SoloEnvironment)
-                return;
-            var songData = SongCore.Collections.RetrieveDifficultyData(difficultyBeatmap);
-            if (songData == null)
-                return;
-            if (songData._colorLeft == null && songData._colorRight == null && songData._envColorLeft == null && songData._envColorRight == null && songData._obstacleColor == null && songData._envColorLeftBoost == null && songData._envColorRightBoost == null)
-                return;
-
-            var environmentInfoSO = difficultyBeatmap.GetEnvironmentInfo();
-            var fallbackScheme = overrideColorScheme ?? new ColorScheme(environmentInfoSO.colorScheme);
-
-            _logger.Info("Custom Song Colors On");
-            var saberLeft = songData._colorLeft == null ? fallbackScheme.saberAColor : ColorFromMapColor(songData._colorLeft);
-            var saberRight = songData._colorRight == null ? fallbackScheme.saberBColor : ColorFromMapColor(songData._colorRight);
-            var envLeft = songData._envColorLeft == null
-                ? songData._colorLeft == null ? fallbackScheme.environmentColor0 : ColorFromMapColor(songData._colorLeft)
-                : ColorFromMapColor(songData._envColorLeft);
-            var envRight = songData._envColorRight == null
-                ? songData._colorRight == null ? fallbackScheme.environmentColor1 : ColorFromMapColor(songData._colorRight)
-                : ColorFromMapColor(songData._envColorRight);
-            var envLeftBoost = songData._envColorLeftBoost == null ? envLeft : ColorFromMapColor(songData._envColorLeftBoost);
-            var envRightBoost = songData._envColorRightBoost == null ? envRight : ColorFromMapColor(songData._envColorRightBoost);
-            var obstacle = songData._obstacleColor == null ? fallbackScheme.obstaclesColor : ColorFromMapColor(songData._obstacleColor);
-            overrideColorScheme = new ColorScheme("SongCoreMapColorScheme", "SongCore Map Color Scheme", true, "SongCore Map Color Scheme", false, saberLeft, saberRight, envLeft,
-                envRight, true, envLeftBoost, envRightBoost, obstacle);
-        }
-
-        private Color ColorFromMapColor(SongCore.Data.ExtraSongData.MapColor mapColor) =>
-            new Color(mapColor.r, mapColor.g, mapColor.b);
     }
 }
